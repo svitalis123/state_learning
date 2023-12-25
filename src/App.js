25ce2b72eb1b0ff262711596f7f12a481ef38dd8
svitalis123 <vitalis.wilson@yahoo.com>
2023-12-25 13:51:05 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          What is Your Favourite day.
+        </h1>
+       <form>
+        <textarea>
+
+        </textarea>
+        <br/>
+        <button>
+          Submit
+        </button>
+       </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
